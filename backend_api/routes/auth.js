11da51a2f21cf0365260f71f4d9cb5d7ee2faae8
zendrex0880 <adversalozen8@gmail.com>
2025-06-15 13:31:02 +0800

    endpoint for sign in

--- backend_api/routes/auth.js
+++ backend_api/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 authRouter.post('/api/signup',async(req,res)=>{
     try{
@@ -23,5 +24,31 @@ authRouter.post('/api/signup',async(req,res)=>{
         res.status(500).json({error:e.message});
     }
 });
+
+authRouter.post('/api/signin',async(req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        const findUser = await User.findOne({email});
+        if(!findUser){
+            return res.status(400).json({msg: "Email do not exist"});
+        }else{
+            const isMatch = await bcrypt.compare(password,findUser.password);
+            if(!isMatch){
+                return res.status(400).json({msg: "Invalid Password"})
+            }else{
+               const token = jwt.sign({id:findUser._id}, "passwordKey");
+               
+               //remove sensitive data from the user object
+               const {password, ...userWithoutPaswword} = findUser._doc;
+               
+                res.json({token, ...userWithoutPaswword});
+            }
+        }
+    }catch(e){
+        res.status(500).json({error:e.message});
+    }
+});
+
 
 module.exports = authRouter;
